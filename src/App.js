dd365eaf5bafc0e1377b84868767bc8aad155002
ebolzan <ebolzan@inf.ufsm.br>
2023-03-07 22:19:46 -0300

    feat(video-list):create video list

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+import VideoList from './components/VideoList';
+
+function App() {
+  return (
+    <div className="App">
+      <VideoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/VideoList.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function VideoList(){
+
+    return(
+        <ul className="video-list">
+            <li className="video">
+                <img src="https://ak.picdn.net/shutterstock/videos/1018031305/thumb/8.jpg" alt="Nada"></img>
+                <div>tock-footageav</div>
+            </li>
+            <li className="video">
+                <img src="https://ak.picdn.net/shutterstock/videos/1018031305/thumb/8.jpg" alt="Nada"></img>
+                <div>tock-footageav</div>
+            </li>
+            <li className="video"> 
+                <img src="https://ak.picdn.net/shutterstock/videos/1018031305/thumb/8.jpg" alt="Nada"></img>
+                <div>tock-footageav</div>
+            </li>
+
+
+        </ul>
+    )
+}
+
+export default VideoList;
